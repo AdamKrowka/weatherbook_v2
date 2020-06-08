2dfac26c6bb97ff83ffe24930ea761ddeac2b7c5
Adam96 <adamkrowka108@gmail.com>
2020-06-08 08:55:59 +0200

    Updates

---
+++ precache-manifest.630d50c0fe211a07b2bd56b282fdfd7a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031e374426aad45224307371ed6cf125",
+    "url": "/weatherbook_v2/index.html"
+  },
+  {
+    "revision": "12c3982eeb7d78e10b6e",
+    "url": "/weatherbook_v2/static/css/main.1a67de50.chunk.css"
+  },
+  {
+    "revision": "cf60bc82854f86ac1c24",
+    "url": "/weatherbook_v2/static/js/2.5491f90e.chunk.js"
+  },
+  {
+    "revision": "2edade83857e9864b99cb4ff5cce8637",
+    "url": "/weatherbook_v2/static/js/2.5491f90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c3982eeb7d78e10b6e",
+    "url": "/weatherbook_v2/static/js/main.a63dd086.chunk.js"
+  },
+  {
+    "revision": "b7a83d94395c750fff8a",
+    "url": "/weatherbook_v2/static/js/runtime-main.471a31d1.js"
+  },
+  {
+    "revision": "06dd61462afc92a9dc9150336f2ccc9e",
+    "url": "/weatherbook_v2/static/media/2682801 - cloudy fog foggy mist moon night weather.06dd6146.svg"
+  },
+  {
+    "revision": "88a27247ee159a9ba666e2715bd96cfe",
+    "url": "/weatherbook_v2/static/media/humidity.88a27247.svg"
+  },
+  {
+    "revision": "086ad671779681f85e74fd30828a8d93",
+    "url": "/weatherbook_v2/static/media/sunrise.086ad671.svg"
+  },
+  {
+    "revision": "842357a96b38b0b1b37bb2999e0a3b2e",
+    "url": "/weatherbook_v2/static/media/sunset.842357a9.svg"
+  },
+  {
+    "revision": "5d743a8b844946a749245e9b52a60940",
+    "url": "/weatherbook_v2/static/media/termometer.5d743a8b.svg"
+  },
+  {
+    "revision": "0487e0f0664d2f49127bdeef7b602af2",
+    "url": "/weatherbook_v2/static/media/wind.0487e0f0.svg"
+  }
+]);
